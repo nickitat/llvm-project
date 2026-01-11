548699d54c17400f664b0f521c400bb317c7f3fa
Nikita Taranov <nikita.taranov@clickhouse.com>
2026-01-11 21:34:00 +0100

    stash

--- clang-tools-extra/std-containers-check/std-containers-check.cpp
+++ clang-tools-extra/std-containers-check/std-containers-check.cpp
@@ -7,6 +7,7 @@
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/SourceLocation.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/CommonOptionsParser.h"
@@ -15,20 +16,97 @@
 // LLVM includes
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/raw_ostream.h"
 
 // Standard includes
 #include <memory>
 
-namespace ClangVariables {
+using namespace clang;
 
-constexpr llvm::StringRef Marker{"clang"};
+using MatchResult = clang::ast_matchers::MatchFinder::MatchResult;
+
+namespace StdContainersCheck {
+
+constexpr llvm::StringRef Marker{"std-containers-check-marker"};
+
+constexpr llvm::StringRef AllowPhrase{"allow-std-containers"};
+
+static std::vector<std::pair<SourceLocation, StringRef>>
+getCommentsInRange(ASTContext *Ctx, CharSourceRange Range) {
+  std::vector<std::pair<SourceLocation, StringRef>> Comments;
+  auto &SM = Ctx->getSourceManager();
+  const std::pair<FileID, unsigned> BeginLoc =
+                                        SM.getDecomposedLoc(Range.getBegin()),
+                                    EndLoc =
+                                        SM.getDecomposedLoc(Range.getEnd());
+
+  llvm::dbgs() << "Searching comments in range: "
+               << Range.getBegin().printToString(SM) << " - "
+               << Range.getEnd().printToString(SM) << "\n";
+  llvm::dbgs() << "Decomposed begin: " << BeginLoc.second << ":"
+               << EndLoc.second << "\n";
+
+  if (BeginLoc.first != EndLoc.first)
+    return Comments;
+
+  bool Invalid = false;
+  const StringRef Buffer = SM.getBufferData(BeginLoc.first, &Invalid);
+  if (Invalid)
+    llvm::report_fatal_error("Error: Unable to retrieve source buffer");
+
+  const char *StrData = Buffer.data() + BeginLoc.second;
+  Lexer TheLexer(SM.getLocForStartOfFile(BeginLoc.first), Ctx->getLangOpts(),
+                 Buffer.begin(), StrData, Buffer.end());
+  TheLexer.SetCommentRetentionState(true);
+
+  while (true) {
+    Token Tok;
+
+    if (TheLexer.LexFromRawLexer(Tok))
+      break;
+
+    // Check if we've gone past the end of our range
+    if (SM.getDecomposedLoc(Tok.getLocation()).second >= EndLoc.second ||
+        Tok.is(tok::eof))
+      break;
+
+    if (Tok.is(tok::comment)) {
+      const auto CommentLoc = SM.getDecomposedLoc(Tok.getLocation());
+      assert(CommentLoc.first == BeginLoc.first);
+      const auto CommentText =
+          StringRef(Buffer.begin() + CommentLoc.second, Tok.getLength());
+      Comments.emplace_back(Tok.getLocation(), CommentText);
+      llvm::dbgs() << "  Found comment: " << CommentText << "; at "
+                   << SM.getDecomposedLoc(Tok.getLocation()).second << "\n";
+    }
+  }
+
+  return Comments;
+}
+
+static CharSourceRange getSourceRange(const MatchResult &Result,
+                                      const clang::NamedDecl &Value) {
+  clang::SourceManager &SM = *Result.SourceManager;
+  const auto MatchBeginLoc = Value.getBeginLoc();
+  const auto MatchBeginLine = SM.getPresumedLoc(MatchBeginLoc).getLine();
+  const auto FileID = SM.getFileID(MatchBeginLoc);
+  const auto SearchBeginLoc =
+      SM.translateLineCol(FileID, MatchBeginLine - 1, 1);
+  const auto MatchEndLine = SM.getPresumedLoc(Value.getEndLoc()).getLine();
+  auto SearchEndLoc = SM.translateLineCol(FileID, MatchEndLine + 1, 1);
+  if (SearchEndLoc.isValid()) {
+    SearchEndLoc = SearchEndLoc.getLocWithOffset(-1);
+  }
+  if (!SearchBeginLoc.isValid() || !SearchEndLoc.isValid()) {
+    llvm::report_fatal_error("Error: Unable to compute search range");
+  }
+  return CharSourceRange::getCharRange(SearchBeginLoc, SearchEndLoc);
+}
 
 class MatchHandler : public clang::ast_matchers::MatchFinder::MatchCallback {
 public:
-  using MatchResult = clang::ast_matchers::MatchFinder::MatchResult;
-
   void run(const MatchResult &Result) override {
     llvm::StringRef Name;
     const clang::NamedDecl *Value = nullptr;
@@ -45,33 +123,39 @@ public:
     } else if ((Value = Result.Nodes.getNodeAs<clang::ParmVarDecl>(Marker))) {
       Name = Value->getName();
     } else {
-      llvm::report_fatal_error(
-          "Error: Matched node is neither VarDecl nor FieldDecl");
+      llvm::report_fatal_error("Error: Matched node is of an unexpected type");
     }
 
-    if (Name.empty() || Name.starts_with("clang_"))
-      return;
+    const auto SourceRange = getSourceRange(Result, *Value);
+    const auto Comments = getCommentsInRange(Result.Context, SourceRange);
+    // If any of the surrounding comments contains the allow phrase, don't
+    // produce a warning.
+    for (const auto &[_, CommentText] : Comments) {
+      if (CommentText.contains(AllowPhrase)) {
+        return;
+      }
+    }
 
-    clang::DiagnosticsEngine &Engine = Result.Context->getDiagnostics();
-    const unsigned ID =
-        Engine.getCustomDiagID(clang::DiagnosticsEngine::Warning,
-                               "clang variable must have 'clang_' prefix");
-
-    const clang::FixItHint FixIt =
-        clang::FixItHint::CreateInsertion(Value->getLocation(), "clang_");
-
-    Engine.Report(Value->getLocation(), ID).AddFixItHint(FixIt);
+    auto &Engine = Result.Context->getDiagnostics();
+    const unsigned ID = Engine.getCustomDiagID(
+        clang::DiagnosticsEngine::Warning,
+        "An alternative specialisation from "
+        "`src/Common/ContainersWithMemoryTracking.h` should be used instead of "
+        "default standard containers. Please, either replace accordingly or "
+        "annotate this usage with a comment containing "
+        "\"allow-std-containers\" string");
+    Engine.Report(Value->getLocation(), ID);
   }
 
 private:
   void printMatches(const MatchResult &Result) {
     for (const auto &[ID, Node] : Result.Nodes.getMap()) {
-      llvm::errs() << "Bound ID: " << ID << "\n";
+      llvm::dbgs() << "Bound ID: " << ID << "\n";
       Node.dump(llvm::errs(), *Result.Context);
-      llvm::errs() << "\n---\n";
+      llvm::dbgs() << "\n---\n";
     }
   }
-}; // namespace ClangVariables
+}; // namespace StdContainersCheck
 
 /// Dispatches the ASTMatcher.
 class Consumer : public clang::ASTConsumer {
@@ -184,10 +268,8 @@ public:
   bool BeginSourceFileAction(clang::CompilerInstance &Compiler) override {
     return true;
   }
-
-  void EndSourceFileAction() override {}
 };
-} // namespace ClangVariables
+} // namespace StdContainersCheck
 
 namespace {
 llvm::cl::OptionCategory ToolCategory("std-containers-check options");
@@ -202,7 +284,7 @@ llvm::cl::extrahelp
     CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
 } // namespace
 
-auto main(int argc, const char *argv[]) -> int {
+int main(int argc, const char *argv[]) {
   using namespace clang::tooling;
 
   auto ExpectedParser = CommonOptionsParser::create(argc, argv, ToolCategory);
@@ -214,6 +296,6 @@ auto main(int argc, const char *argv[]) -> int {
   ClangTool Tool(OptionsParser.getCompilations(),
                  OptionsParser.getSourcePathList());
 
-  const auto Action = newFrontendActionFactory<ClangVariables::Action>();
+  const auto Action = newFrontendActionFactory<StdContainersCheck::Action>();
   return Tool.run(Action.get());
 }
